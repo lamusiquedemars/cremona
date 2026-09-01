3dd3e7e2918fa605900c2578243e9d57713c44f3
lamusiquedemars <ivocore@gmail.com>
2026-09-01 15:59:34 +0200

    revert: keep established Google Ads token handling

--- app/Services/GoogleAdsApiClient.php
+++ app/Services/GoogleAdsApiClient.php
@@ -63,10 +63,7 @@ class GoogleAdsApiClient
 
         $this->accessToken = $token;
 
-        $request = Http::acceptJson()->withHeaders([
-            'Authorization' => 'Bearer '.$token,
-            'developer-token' => $this->credentials['developer_token'],
-        ])->timeout(30);
+        $request = Http::acceptJson()->withToken($token)->withHeader('developer-token', $this->credentials['developer_token'])->timeout(30);
 
         return filled($this->credentials['login_customer_id'] ?? null)
             ? $request->withHeader('login-customer-id', preg_replace('/\D/', '', $this->credentials['login_customer_id']))
