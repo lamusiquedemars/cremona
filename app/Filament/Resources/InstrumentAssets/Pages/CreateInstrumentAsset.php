de261ee8daea2ae80324e49a1854e0f099b69203
lamusiquedemars <ivocore@gmail.com>
2026-09-18 14:33:28 +0200

    fix: publish instruments on creation

--- app/Filament/Resources/InstrumentAssets/Pages/CreateInstrumentAsset.php
+++ app/Filament/Resources/InstrumentAssets/Pages/CreateInstrumentAsset.php
@@ -4,8 +4,32 @@ namespace App\Filament\Resources\InstrumentAssets\Pages;
 
 use App\Filament\Resources\InstrumentAssets\InstrumentAssetResource;
 use App\Filament\Pages\BusinessCreateRecord;
+use App\Services\ContempoInstrumentPublisher;
+use Filament\Notifications\Notification;
+use LogicException;
 
 class CreateInstrumentAsset extends BusinessCreateRecord
 {
     protected static string $resource = InstrumentAssetResource::class;
+
+    protected function afterCreate(): void
+    {
+        if (! $this->record->is_site_published) {
+            return;
+        }
+
+        try {
+            app(ContempoInstrumentPublisher::class)->publish($this->record);
+            Notification::make()
+                ->title('Site synchronisé')
+                ->success()
+                ->send();
+        } catch (LogicException $exception) {
+            Notification::make()
+                ->title('Mise à jour du site interrompue')
+                ->body($exception->getMessage())
+                ->danger()
+                ->send();
+        }
+    }
 }
